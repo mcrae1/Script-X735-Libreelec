a2c5868180fe130ca6cc9121ccc9633ed5209d98
Andreas Neubert <a.neubert@t-online.de>
2024-06-15 14:00:25 +0200

    - hotfix for shell scripts to use with LE12/LE13 - simple script header with version information and changelog added - README.md updated - file comandos.txt renamed to English meaning

--- shutdown_x735.sh
+++ shutdown_x735.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
+# Since kernel version 6.5.x sysfs-gpio is deprecated and could be
+# a breaking change in the future too. Also the gpiochip numbering was
+# unified for all platforms. Now it's needed to use another offset to
+# address the GPIOs at the 40-pin header of the RPi.
+# With older kernel versions the offset was 0. Now the offset is: 512
+# The design of RPi5 uses the new RP1 I/O chip and the offset differs
+# to previous RPi generations: 571
+#
+# GPIO4 + Offset = GPIO516
+# GPIO17 + Offset = GPIO529
+#
+# Changed on: 2024-06-15 11:37
+# Version: 0.0.1
+#
+# Changelog:
+# 0.0.1 2024-06-15
+# - offset of 512 for RPi4 added to the GPIO number
+# - cosmetic changes
 
-SHUTDOWN=4
+SHUTDOWN=516 # GPIO 4
 REBOOTPULSEMINIMUM=200
 REBOOTPULSEMAXIMUM=600
 echo "$SHUTDOWN" > /sys/class/gpio/export
 echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
-BOOT=17
+BOOT=529 # GPIO 17
 echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
@@ -21,18 +39,16 @@ while [ 1 ]; do
     while [ $shutdownSignal = 1 ]; do
       /bin/sleep 0.02
       if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
-        echo "X735 Shutting down", SHUTDOWN, ", halting Rpi ..."
+        echo "X735 Shutting down", SHUTDOWN, ", halting RPi ..."
         poweroff
         exit
       fi
       shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
     done
     if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then
-      echo "X735 Rebooting", SHUTDOWN, ", recycling Rpi ..."
+      echo "X735 Rebooting", SHUTDOWN, ", restarting RPi ..."
       reboot
       exit
     fi
   fi
 done
-
-
--- softshutdown.sh
+++ softshutdown.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-BUTTON=18
+# Since kernel version 6.5.x sysfs-gpio is deprecated and could be
+# a breaking change in the future too. Also the gpiochip numbering was
+# unified for all platforms. Now it's needed to use another offset to
+# address the GPIOs at the 40-pin header of the RPi.
+# With older kernel versions the offset was 0. Now the offset is: 512
+# The design of RPi5 uses the new RP1 I/O chip and the offset differs
+# to previous RPi generations: 571
+#
+# GPIO18 + Offset = GPIO530
+#
+# Changed on: 2024-06-15 11:40
+# Version: 0.0.1
+#
+# Changelog:
+# 0.0.1 2024-06-15
+# - offset of 512 for RPi4 added to the GPIO number
+
+BUTTON=530 # GPIO 18
 
 #setup GPIO 18 as output and set to HIGH
 echo "$BUTTON" > /sys/class/gpio/export;
